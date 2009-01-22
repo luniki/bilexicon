2209eb53c4d7e87a27df1c06c4b3c1c3b0391bf5
Marcus Lunzenauer <mlunzena@uos.de>
2009-01-22 15:19:34 +0100

    change speedy

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,21 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+(function(){
+  var methods = {
+    defaultValueActsAsHint: function(element){
+      element = $(element);
+      element._default = element.value;
+
+      return element.observe('focus', function(){
+        if(element._default != element.value) return;
+        element.removeClassName('hint').value = '';
+      }).observe('blur', function(){
+        if(element.value.strip() != '') return;
+        element.addClassName('hint').value = element._default;
+      }).addClassName('hint');
+    }
+  };
+
+  $w('input textarea').each(function(tag){ Element.addMethods(tag, methods) });
+})();
