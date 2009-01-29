7703910e3ce75721f4b298b738b36f579bbd9bfa
Marcus Lunzenauer <mlunzena@uos.de>
2009-01-29 12:29:30 +0100

    add hoptoad notifier

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,14 +2,16 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include HoptoadNotifier::Catcher
+
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'a6574404e21c90a90e9518b027d67123'
-  
-  # See ActionController::Base for details 
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # from your application log (in this case, all fields with names like "password").
   # filter_parameter_logging :password
 end
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '477580ec993810486fadddbd43dfc9bd'
+end
