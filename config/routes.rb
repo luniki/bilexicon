f8d0156f683e88a1ca52cc0b037334db695dfbda
Marcus Lunzenauer <mlunzena@uos.de>
2009-03-03 20:58:28 +0100

    add basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
 
+  map.resource :user_session
+#  map.root :controller => "user_sessions", :action => "new"
+
+  map.resource :account, :controller => "users"
+  map.resources :users
+
+
   map.resources :categories
 
   map.resources :lemmata do |lemma|
