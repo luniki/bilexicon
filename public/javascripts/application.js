04677de672addd278d547f87c2b36a4d740e745c
Marcus Lunzenauer <mlunzena@uos.de>
2009-03-31 11:40:44 +0200

    add popup search for other dictionaries

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -19,3 +19,43 @@
 
   $w('input textarea').each(function(tag){ Element.addMethods(tag, methods) });
 })();
+
+
+/* ------------------------------------------------------------------------
+ * the global BILEXICON namespace
+ * ------------------------------------------------------------------------ */
+if (typeof BILEXICON == "undefined" || !BILEXICON) {
+    var BILEXICON = {};
+}
+
+/* ------------------------------------------------------------------------
+ * popup for other search engines on lemmata search page
+ * ------------------------------------------------------------------------ */
+BILEXICON.SearchPopup = function(event) {
+
+    event.stop();
+
+    var q = $('search_term').innerHTML;
+    var increment = 20;
+    var popup = function() {
+      ['http://pons.eu/dict/search/results/?l=deen&q=',
+        'http://dict.leo.org/ende?search=',
+        'http://www.dict.cc/?s=',
+        'http://dict.tu-chemnitz.de/dings.cgi?query='].reverse().each(
+        function(url) {
+          window.open(url + encodeURIComponent(q), url.gsub(/\W/, ""),
+                      "height=400,width=600,top=" + increment +",left=" + increment);
+          increment += 25;
+        }
+      );
+    };
+
+    popup();
+};
+
+/* ------------------------------------------------------------------------
+ * dom:loaded events
+ * ------------------------------------------------------------------------ */
+document.observe("dom:loaded", function() {
+  $("popup-search").observe("click", BILEXICON.SearchPopup);
+});
