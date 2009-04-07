ff9c2ad8d01053cebdc9cc501c60b4120d98d5a8
Marcus Lunzenauer <mlunzena@uos.de>
2009-04-07 09:18:43 +0200

    additional fields to examples

--- db/migrate/20081211130226_create_examples.rb
+++ db/migrate/20081211130226_create_examples.rb
@@ -3,6 +3,11 @@ class CreateExamples < ActiveRecord::Migration
     create_table :examples do |t|
       t.string :form1
       t.string :form2
+      t.string :synonym1
+      t.string :synonym2
+      t.string :syntax1
+      t.string :syntax2
+      t.string :form2
       t.integer :exampleable_id
       t.string :exampleable_type
 
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(:version => 20090403074227) do
   create_table "examples", :force => true do |t|
     t.string   "form1"
     t.string   "form2"
+    t.string   "synonym1"
+    t.string   "synonym2"
+    t.string   "syntax1"
+    t.string   "syntax2"
     t.integer  "exampleable_id"
     t.string   "exampleable_type"
     t.datetime "created_at"
