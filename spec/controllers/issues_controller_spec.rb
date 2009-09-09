603710052ac215b77c42d4f6933a170ad68ac667
Marcus Lunzenauer <mlunzena@uos.de>
2009-09-09 16:25:04 +0200

    update issues controller spec

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -2,11 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe IssuesController do
 
-  #Delete these examples and add some real ones
-  it "should use IssuesController" do
-    controller.should be_an_instance_of(IssuesController)
-  end
-
+  it_should_behave_like "an authenticated controller"
 
   describe "GET 'new'" do
     it "should be successful" do
