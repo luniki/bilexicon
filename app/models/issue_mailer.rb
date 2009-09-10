aa02733afdacbac410f2cdd0fc938f2b392012f6
Marcus Lunzenauer <mlunzena@uos.de>
2009-09-10 15:33:19 +0200

    use real email adress

--- app/models/issue_mailer.rb
+++ app/models/issue_mailer.rb
@@ -2,7 +2,7 @@ class IssueMailer < ActionMailer::Base
 
   def issue(issue)
     recipients    APP_CONFIG["site_owner"]["email"]
-    from          "Bilexicon <no-reply@#bilexicon>"
+    from          "no-reply@bilexicon"
     subject       "Issue: #{issue[:title]}"
     sent_on       Time.now
     body          issue
