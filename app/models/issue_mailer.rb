c657f5c29b16912e54ea6e2f9a7d92cdf0755bbe
Marcus Lunzenauer <mlunzena@uos.de>
2009-09-12 12:39:01 +0200

    fix issue mailer

--- app/models/issue_mailer.rb
+++ app/models/issue_mailer.rb
@@ -2,7 +2,7 @@ class IssueMailer < ActionMailer::Base
 
   def issue(issue)
     recipients    APP_CONFIG["site_owner"]["email"]
-    from          "no-reply@bilexicon"
+    from          "no-reply@" + Socket.gethostname
     subject       "Issue: #{issue[:title]}"
     sent_on       Time.now
     body          issue
