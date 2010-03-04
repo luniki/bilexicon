aaa0fe3d607564d9fa6611382ff4738705a10617
Marcus Lunzenauer <mlunzena@uos.de>
2010-03-04 13:31:22 +0100

    let users create decks

--- features/plain/create_deck_from_category.feature
+++ features/plain/create_deck_from_category.feature
@@ -7,5 +7,18 @@ Feature: create decks of flashcards from categories
 Scenario: a "create" button is shown authenticated learners
   Given a category named "Travel & Transport"
   When I go to the category "Travel & Transport"
-  Then I should see a button to create a new deck
+  Then I should see a link to create a new deck
+
+@authenticated @wip
+Scenario: the "create" button should transport the learner to the "new deck" page
+  Given a category named "Travel & Transport"
+  When I go to the category "Travel & Transport"
+  And I follow "create_deck"
+  Then I should see a text field containing "Travel & Transport"
+
+@authenticated @wip
+Scenario: the "create" button should transport the learner to the "new deck" page
+  Given a category named "Travel & Transport"
+  When I create a new deck from the category "Travel & Transport"
+  Then I should see a a new deck named "Travel & Transport"
 
--- features/step_definitions/deck_steps.rb
+++ features/step_definitions/deck_steps.rb
@@ -1,9 +1,22 @@
 Given /^a category named "([^\"]*)"$/ do |name|
   @category = Factory.create(:category, :name => name)
-  pp @category
 end
 
-Then /^I should see a button to create a new deck$/ do
-  response_body.should have_tag "form[action=?]", new_user_deck_path
+Then /^I should see a link to create a new deck$/ do
+  response_body.should have_tag "a[href=?]", new_user_deck_path(@user, :category_id => @category)
+end
+
+Then /^I should see a text field containing "([^\"]*)"$/ do |content|
+  response_body.should have_tag "input[id=deck_name][value=?]", ERB::Util.html_escape(content)
+end
+
+Then /^I should see a a new deck named "([^\"]*)"$/ do |name|
+  response_body.should have_tag "h1", /#{ERB::Util.html_escape(name)}/
+end
+
+When /^I create a new deck from the category "([^\"]*)"$/ do |name|
+  When %{I go to the category "#{name}"}
+  And %{I follow "create_deck"}
+  And %{I press "create"}
 end
 
