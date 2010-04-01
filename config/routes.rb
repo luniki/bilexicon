025e9a99ed4d06674cc473be858fcda08670562c
Marcus Lunzenauer <mlunzena@uos.de>
2010-04-01 15:26:58 +0200

    make  categories the entry page

--- config/routes.rb
+++ config/routes.rb
@@ -74,7 +74,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "lemmata"
+  map.root :controller => "categories"
 
   # See how all your routes lay out with "rake routes"
 
